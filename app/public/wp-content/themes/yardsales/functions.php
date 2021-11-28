0cf01f2a7935698abf2257a7b81530805516efa9
Adolfo Lopez <lopezc.adolfo@gmail.com>
2021-11-28 16:34:59 -0500

    Dinamismo basico con theme_supports

--- app/public/wp-content/themes/yardsales/functions.php
+++ app/public/wp-content/themes/yardsales/functions.php
@@ -32,3 +32,16 @@ function plz_assets()
 }
 
 add_action("wp_enqueue_scripts", "plz_assets");
+
+function plz_theme_supports(){
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    add_theme_support('custom-logo',array(
+        "width" => 170,
+        "height" => 35,
+        "flex-width" => true,
+        "flex-height" => true
+    ));
+}
+
+add_action("after_setup_theme","plz_theme_supports");   
--- app/public/wp-content/themes/yardsales/header.php
+++ app/public/wp-content/themes/yardsales/header.php
@@ -1,30 +1,25 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo("charset") ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <title>Yards Sales</title>
     <?php wp_head(); ?>
 </head>
 
 <body>
-
+    <?php wp_body_open(); ?>
     <header class="encabezado">
         <div class="container-fluid gx-5 py-3">
             <div class="row">
                 <div class="encabezado__hamburguesa col-2">
-                    <a href="#">
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icon-menu.svg" alt="menu icon">
-                    </a>
+                    <?php the_custom_logo(); ?>
                 </div>
-                <div class="enbezado__logo col-5 col-md-2 px-2 px-md-4">
-                    <a href="/">
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" width="100%" alt="Yard Sales">
-                    </a>
+                <div class="encabezado__logo col-5 col-md-2 px-2 px-md-4">
+                    <?php the_custom_logo(); ?>
                 </div>
                 <div class="encabezado__menu col-6 col-md-5">
                     <ul>
