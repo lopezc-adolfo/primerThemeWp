faa49d697610627d2d41c13261363779e7a8617d
Adolfo Lopez <lopezc.adolfo@gmail.com>
2021-11-27 19:18:17 -0500

    Hooks, incluir archivos css y js por medio de un hook en functions.php

--- app/public/wp-content/themes/yardsales/footer.php
+++ app/public/wp-content/themes/yardsales/footer.php
@@ -1,10 +1,10 @@
 </div>
-    </main>
+</main>
 
-    <footer class="text-center my-4">
-        De LATAM con ♥ para el mundo
-    </footer>
-<script src="assets/js/script.js"></script>
+<footer class="text-center my-4">
+    De LATAM con ♥ para el mundo
+</footer>
+<?php wp_footer(); ?>
 </body>
 
 </html>
